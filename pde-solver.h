8c4e5f4a4a31b00f799d17ae8120f49fbd2f0ea7
Julius Sellmayer <ge35kek@mytum.de>
2025-11-28 15:57:31 +0100

    Edit pde-solver.h

--- pde-solver.h
+++ pde-solver.h
@@ -5,6 +5,7 @@
 
 // function declarations
 void set_mesh(int& N, int& M, double& Lx, double& Ly);
+void set_mesh_min(std::vector<std::vector<double>>& mesh, int nx, int ny, double t_avg);
 void set_boundaries(std::vector<std::vector<double>>& U, int N, int M);
 void solve_steady_state(std::vector<std::vector<double>>& U, int N, int M, int max_iter = 1000, double tolerance = 1e-3);
 void save_to_file(std::vector<std::vector<double>>& U, int N, int M, double Lx, double Ly);
