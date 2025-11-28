91170e19dfd6d79d121e6baa0ee901ba835ccf61
Julius Sellmayer <ge35kek@mytum.de>
2025-11-28 01:43:19 +0100

    not able to pull the other branch :( functionable solver, outputs to csv file need to add cmakelist: cmake_minimum_required(VERSION 4.0) project(-project-)
    set(CMAKE_CXX_STANDARD 23)
    
    add_executable(-project- main.cpp pde-solver.cpp pde-solver.h)

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "pde-solver.h"
+int main() {
+    //Define parameters
+    //nx, ny: points on mesh in x-, y-direction
+    //lx, ly: length of mesh in x-, y-direction
+    int nx{0};
+    int ny{0};
+    double lx{0.0};
+    double ly{0.0};
+    set_mesh(nx, ny, lx, ly);
+    //Define stepsize
+    const double dx = double(lx) / (nx - 1);
+    const double dy = double(ly) / (ny - 1);
+    //Initialize mesh
+    std::vector<std::vector<double>> mesh(nx, std::vector<double>(ny, 0));
+    //set boundaries
+    set_boundaries(mesh, nx, ny);
+    //std::cout << "testout";
+    //solve for steady state
+    solve_steady_state(mesh, nx, ny);
+    //save to csv file "steady_state_sol.csv"
+    save_to_file(mesh, nx, ny, dx, dy);
+
+    //test
+    //std::cout << nx << '\n' << ny << '\n' << lx << '\n' << ly << '\n' << dx << '\n' << dy << '\n';
+
+    return 0;
+}
